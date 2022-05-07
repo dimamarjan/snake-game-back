bb26b0e8fd74cdae213505e10a2457c3baaa5168
Dmitriy Marianchenko <dima.marjan@gmail.com>
2022-05-07 23:12:24 +0300

    upd heroku

--- db/db.js
+++ db/db.js
@@ -3,16 +3,13 @@ require("dotenv").config();
 
 const { HERPKU_DB_URL } = process.env;
 
-const db = new Sequelize(
-    "postgres://cstyxpbqzbdtmn:c012f224c8abb718be05542509b783536689808a10d242c87f8ff4b8daa7b6eb@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/dchb1vrd2s4sjm",
-    {
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false,
-            },
+const db = new Sequelize(HERPKU_DB_URL, {
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
         },
-    }
-);
+    },
+});
 
 module.exports = db;
