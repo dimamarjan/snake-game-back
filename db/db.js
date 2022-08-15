504308236bfb065c8aed25f50c9af7d10cbedb6c
dimamarjan <dima.marjan@gmail.com>
2022-08-15 22:35:35 +0300

    updated

--- db/db.js
+++ db/db.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const { HERPKU_DB_URL } = process.env;
+const HEROKU_DB_URL = process.env.HEROKU_DB_URL;
 
-const db = new Sequelize(HERPKU_DB_URL, {
+const db = new Sequelize(HEROKU_DB_URL, {
     dialectOptions: {
         ssl: {
             require: true,
